03600ab9ff32b01771043cf845f6231f4d29b43e
Manoj <manojparasuram.sadhanala@gmail.com>
2021-12-27 14:59:59 +0530

    Functions

---
+++ Chapter-03/TheCollatzSequence.py
@@ -0,0 +1,11 @@
+def collatzseq(n):
+    while n!=1:
+        if n%2==0:
+            n = n//2
+        else:
+            n = (3*n)+1
+        print(n)
+
+print('Enter number:')
+n = int(input())
+collatzseq(n)
