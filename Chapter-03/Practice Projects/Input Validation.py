abc6bbe3e12cea4aceab141e6ab4acee299bccd2
Manoj <manojparasuram.sadhanala@gmail.com>
2021-12-28 17:42:47 +0530

    Functions

---
+++ Chapter-03/Practice Projects/Input Validation.py
@@ -0,0 +1,17 @@
+#collatz sequence
+def collatzseq(n):
+    while n!=1:
+        if n%2==0:
+            n = n//2
+        else:
+            n = (3*n)+1
+        print(n)
+x = 0
+while x==0:
+    print('Enter number:')
+    try:
+        n = int(input())
+        x+=1
+    except ValueError:
+        print('Error: Given argument is invalid, Only integers')
+collatzseq(n)
